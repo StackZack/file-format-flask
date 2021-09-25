646a7290f1fd1d0133d588c05b5da01c8eb3aa0a
Zackary Luedtke <luedtkezack@gmail.com>
2021-09-25 13:51:14 -0500

    Created simple blueprint for reading json

--- file-format-flask/__init__.py
+++ file-format-flask/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
+from .json_reader import json_reader_bp
 
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def hello_world():
-        return "<p>Hello, World!</p>"
+    app.register_blueprint(json_reader_bp)
 
     return app
